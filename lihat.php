b59ba81f211089771c66aeed63b304ecdea05e93
17191261Muammarnuraulia <101696071+17191261Muammarnuraulia@users.noreply.github.com>
2022-06-15 16:53:59 +0700

    perbaikan kesalahan
    lihat.php

--- lihat.php
+++ lihat.php
@@ -33,6 +33,7 @@
 						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 							<span aria-hidden="true">&times;</span>
 							
+							
 						</button>
 					</div>
 				<?php elseif($this->session->flashdata('error')) : ?>
